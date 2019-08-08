f2760ea54ee9a440f026de52da155434350bc121
Tim Lolkema <30321659+tlolkema@users.noreply.github.com>
2019-08-08 16:00:21 +0200

    CHA - Refactor and more sample usage documentation

--- locust_compare.py
+++ locust_compare.py
@@ -1,14 +1,34 @@
 """
-LocustCompare
+Locust-Compare
 
 This script is used to compare the results of the previous Locust run with the current one.
 This can be used in a Jenkins pipeline to determine if you want to pass/fail the build step based on the differences.
 
+A Locust run outputs 2 csv files:
+prefix_distribution.csv
+prefix_requests.csv
+
+With Locust-Compare you can rename these files to a baseline.
+When you have a baseline you can perform a new Locust run.
+With Locust-Compare you can compare and validate these to the baseline.
+
 The dependencies are python3, pandas.
 
-Sample usage:
+Sample usage: create baseline
+  $ python3 locust_compare.py --prefix example --option create_baseline
+
+Sample usage: create merged results csv:
   $ locust -f example_test.py --csv=example --host https://www.github.com --no-web -t 1m
-  $ python3 locust_compare.py --prefix example --option write_previous_results
+  $ python3 locust_compare.py --prefix example --option create_baseline
+  $ locust -f example_test.py --csv=example --host https://www.github.com --no-web -t 1m
+  $ python3 locust_compare.py --prefix example --option create_comparison_requests
+
+Sample usage: CI/CI compare against previous run (make sure you have atleast 1 locust run)
+  $ python3 locust_compare.py --prefix example --option create_baseline
+  $ locust -f example_test.py --csv=example --host https://www.github.com --no-web -t 1m
+  $ python3 locust_compare.py --prefix example --option compare_results_distribution --columnname 95% --factor 1.2
+
+Sample usage: CI/CD compare against baseline (make sure you have your baseline created)
   $ locust -f example_test.py --csv=example --host https://www.github.com --no-web -t 1m
   $ python3 locust_compare.py --prefix example --option compare_results_distribution --columnname 95% --factor 1.2
 
@@ -26,39 +46,38 @@ class LocustCompare:
     def __init__(self, prefix):
         self._prefix = prefix
 
-    def write_previous_results(self):
+    def create_baseline(self):
         if os.path.exists(f'{self._prefix}_distribution_previous.csv'):
-            os.remove(f'{self._prefix}_distribution_previous.csv')
-            os.remove(f'{self._prefix}_requests_previous.csv')
-        elif os.path.exists(f'{self._prefix}_distribution.csv'):
+            if os.path.exists(f'{self._prefix}_distribution.csv'):
+                os.remove(f'{self._prefix}_distribution_previous.csv')
+                os.remove(f'{self._prefix}_requests_previous.csv')
+                print('Removed old baseline')
+        if os.path.exists(f'{self._prefix}_distribution.csv'):
             os.rename(f'{self._prefix}_distribution.csv', f'{self._prefix}_distribution_previous.csv')
             os.rename(f'{self._prefix}_requests.csv', f'{self._prefix}_requests_previous.csv')
-        else:
+            print('Created new baseline')
+        elif not os.path.exists(f'{self._prefix}_distribution_previous.csv'):
             sys.exit(
                 f'An error occured\n'
                 f'Make sure you first run a Locust test\n'
-                f'Missing: {self._prefix}_distribution.csv'
+                f'Missing: {self._prefix}_distribution_previous.csv'
             )
+        print('Baseline exists')
 
-    def compare_results_distribution(self, columnname, factor):
+    def compare_results(self, columnname, factor, rtype):
         column = columnname
         fac = float(factor)
-        df = self.return_comparison_distribution()
-        df_columns = df[['Name',f'{columnname}_new',f'{columnname}_old']]
-        f = df_columns[f'{columnname}_new'] / df_columns[f'{columnname}_old']
-        self.validate_results(f, fac, df_columns)
-
-    def compare_results_requests(self, columnname, factor):
-        column = columnname
-        fac = float(factor)
-        df = self.return_comparison_requests()
+        if rtype == 'distribution': 
+            df = self.return_comparison_distribution()
+        elif rtype == 'requests':
+            df = self.return_comparison_requests()
         df_columns = df[['Name',f'{columnname}_new',f'{columnname}_old']]
         f = df_columns[f'{columnname}_new'] / df_columns[f'{columnname}_old']
         self.validate_results(f, fac, df_columns)
 
     def validate_results(self, f, fac, df_columns):
         if (f > fac).any():
-            sys.exit(
+            raise AssertionError(
                 f'One of the requests is above the given treshold factor\n'
                 f'Columns: {df_columns}\n'
                 f'Given Factor: {fac}\n'
@@ -124,12 +143,12 @@ if __name__ == '__main__':
 
     compare = LocustCompare(args.prefix)
 
-    if args.option == 'write_previous_results':
-        compare.write_previous_results()
+    if args.option == 'create_baseline':
+        compare.create_baseline()
     elif args.option == 'compare_results_distribution':
-        compare.compare_results_distribution(args.columnname,args.factor)
+        compare.compare_results(args.columnname,args.factor,'distribution')
     elif args.option == 'compare_results_requests':
-        compare.compare_results_requests(args.columnname,args.factor)
+        compare.compare_results(args.columnname,args.factor,'requests')
     elif args.option == 'create_comparison_distribution':
         compare.create_comparison_distribution()
     elif args.option == 'create_comparison_requests':
